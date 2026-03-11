1abb2ee3a5d56ab5fe7af8d917481a160acdbd26
Kanishkau4 <kanishkau22@gmail.com>
2026-03-11 07:12:48 +0530

    feat: Add initial UI and styling for recipe app core screens and components.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Edit app/index.tsx to edit this screen 123.</Text>
+
+      <Image source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }} style={{ width: 100, height: 100 }} />
+      <TouchableOpacity>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+      <Link href="/about">
+        <Text>About Screen</Text>
+      </Link>
     </View>
   );
 }
