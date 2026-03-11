418d64bfa84813359d6999191291e922c94ca160
Kanishkau4 <kanishkau22@gmail.com>
2026-03-11 16:21:15 +0530

    feat(auth): add sign-in screen and logic
    - Implemented sign-in UI with form fields
    - Added Clerk sign-in logic and error handling
    - Removed SafeScreen, used

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,9 +1,116 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert, KeyboardAvoidingView, Platform, TextInput, ScrollView, Image } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { useSignIn } from '@clerk/expo';
+import { authStyles } from '../../assets/styles/auth.styles';
+import { COLORS } from '../../constants/colors';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 const SignInScreen = () => {
+  const router = useRouter();
+
+  const { signIn, setActive, isLocated } = useSignIn();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      Alert.alert('Error', 'Please enter your email and password.');
+    } else if (!isLocated) {
+      Alert.alert('Error', 'Please allow location access to use this feature.');
+      return;
+    }
+
+    if (!isLoaded) return;
+    setLoading(true);
+
+    try {
+      const signInAttempt = await signIn.start({
+        identifier: email,
+        password: password,
+      });
+      if (signInAttempt.status === 'complete') {
+        router.push('/');
+      } else {
+        Alert.alert('Error', 'Sign in failed. Please try again later.');
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Sign in failed. Please try again later.');
+      console.error(JSON.stringify(error, null, 2));
+    }
+  };
+
   return (
-    <View>
-      <Text>Sign In Screen</Text>
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        style={authStyles.keyboardView}
+        behavior={Platform.OS === 'android' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'android' ? 10 : 0}
+      >
+        <ScrollView
+          contentContainerStyle={authStyles.scrollContainer}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={authStyles.imageContainer}>
+            <Image source={require('../../assets/images/p1.png')} style={authStyles.image} contentFit="contain" />
+          </View>
+          <Text style={authStyles.title}>Welcome back</Text>
+          {/* FORM CONTAINER */}
+          <View style={authStyles.formContainer}>
+            <TextInput
+              style={authStyles.textInput}
+              placeholder="Enter your email"
+              placeholderTextColor={COLORS.textLight}
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+          {/*PASSWORD INPUT*/}
+          <View style={authStyles.formContainer}>
+            <TextInput
+              style={authStyles.textInput}
+              placeholder="Enter your password"
+              placeholderTextColor={COLORS.textLight}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+              autoCapitalize="none"
+            />
+            <TouchableOpacity
+              style={authStyles.eyeIcon}
+              onPress={() => setShowPassword(!showPassword)}
+            >
+              <Ionicons
+                name={showPassword ? 'eye-off' : 'eye'}
+                size={20}
+                color={COLORS.textLight}
+              />
+            </TouchableOpacity>
+          </View>
+          {/*SIGN IN BUTTON*/}
+          <TouchableOpacity style={[authStyles.authButton, loading && authStyles.authButtonDisabled]}
+            onPress={handleSignIn}
+            disabled={loading}
+            activeOpacity={0.8}
+          >
+            <Text style={authStyles.buttonText}>{loading ? 'Signing in...' : 'Sign In'}</Text>
+          </TouchableOpacity>
+
+          {/*SIGN UP LINK*/}
+          <TouchableOpacity
+            style={authStyles.linkContainer}
+            onPress={() => router.push('/(auth)/sign-up')}>
+            <Text style={authStyles.linkText}>
+              Don't have an account? <Text style={authStyles.link}>Sign Up</Text></Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 }
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,23 @@
 import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
-import SafeScreen from "@/components/SafeScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "../constants/colors"
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <SafeScreen>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
         <Slot />
-      </SafeScreen>
+      </SafeAreaView>
     </ClerkProvider>
   );
 }
